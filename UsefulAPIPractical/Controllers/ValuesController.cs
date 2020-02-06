c6726a5425a3a7194e6c40874386bcc6182c7ace
Nidhi <nidhi.mvc@gmail.com>
2020-02-06 09:31:02 +0530

    .

--- UsefulAPIPractical/Controllers/ValuesController.cs
+++ UsefulAPIPractical/Controllers/ValuesController.cs
@@ -1,39 +1,83 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using UsefulAPIPractical.Utility;
 
 namespace UsefulAPIPractical.Controllers
 {
     public class ValuesController : ApiController
     {
-        // GET api/values
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+        // get the session id first
+        //string cookie = getSessionId();
+        //object GetSourceList()
+        //{
+        //    using (var httpClient = new HttpClient())
+        //        httpClient.BaseAddress = new UriBuilder("http://localhost:63586").Uri;
+        //    var message = new HttpRequestMessage(HttpMethod.Get, "/api/sources");
+        //    // add cookie value to the message header
+        //    message.Headers.Add("cookie", cookie);
+        //    // send to server
+        //    var result = httpClient.SendAsync(message);
+        //    result.Result.EnsureSuccessStatusCode();
+        //    // get json response
+        //    var s = result.Result.Content.ReadAsStringAsync().Result;
+        //    return JsonConvert.DeserializeObject(s);
+        //}
 
-        // GET api/values/5
-        public string Get(int id)
-        {
-            return "value";
-        }
+        //public string getSessionId()
+        //{
+        //    using (var httpClient = new HttpClient())
+        //    {
+        //        httpClient.BaseAddress = new UriBuilder("http://localhost:63586").Uri;
+        //        dynamic loginInfo = new ExpandoObject();
+        //        loginInfo.user = "nidhi.t@elaunchinfotech.com";
+        //        loginInfo.password = "N@userful1";
+        //        var auth = JsonConvert.SerializeObject(loginInfo);
+        //        var buffer = System.Text.Encoding.UTF8.GetBytes(auth);
+        //        var byteContent = new ByteArrayContent(buffer);
+        //        var ret = httpClient.PostAsync("http://localhost:63586/api/system", byteContent);
+        //        IEnumerable<string> retval;
+        //        ret.Result.Headers.TryGetValues("Set-Cookie", out retval);
+        //        if (retval != null)
+        //            return retval.FirstOrDefault();
+        //        else
+        //            return null;
+        //    }
+        //}
 
-        // POST api/values
-        public void Post([FromBody]string value)
+        public ApiResponse GetData()
         {
-        }
-
-        // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        public void Delete(int id)
-        {
+            ApiResponse apiResponse = new ApiResponse();
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    httpClient.BaseAddress = new UriBuilder("http://localhost:63586").Uri;
+                    dynamic loginInfo = new ExpandoObject();
+                    loginInfo.user = "nidhi.t@elaunchinfotech.com";
+                    loginInfo.password = "N@userful1";
+                    var auth = JsonConvert.SerializeObject(loginInfo);
+                    var buffer = System.Text.Encoding.UTF8.GetBytes(auth);
+                    var byteContent = new ByteArrayContent(buffer);
+                    var ret = httpClient.PostAsync("api/system", byteContent);
+                    IEnumerable<string> retval;
+                    ret.Result.Headers.TryGetValues("Set-Cookie", out retval);
+                    if (retval != null)
+                        apiResponse = Common.GenerateApiResponse(1, true, "No data available", retval.FirstOrDefault());
+                    else
+                        apiResponse = Common.GenerateApiResponse(0, false, "Something went wrong", null);
+                }
+            }
+            catch(Exception)
+            {
+                apiResponse = Common.GenerateApiResponse(0, false, "Something went wrong", null);
+            }
+            return apiResponse;
         }
     }
 }
---
+++ UsefulAPIPractical/Utility/Common.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UsefulAPIPractical.Utility
+{
+    public static class Common
+    {
+        public static ApiResponse GenerateApiResponse(int ResultCode, bool Status, string Message, object Data)
+        {
+            ApiResponse apiResponse = new ApiResponse();
+            apiResponse.ResultCode = ResultCode;
+            apiResponse.Status = Status;
+            apiResponse.Message = Message;
+            apiResponse.Data = Data;
+            return apiResponse;
+        }
+    }
+
+    public class ApiResponse
+    {
+        public int ResultCode { get; set; }
+        public bool Status { get; set; }
+        public string Message { get; set; }
+        public object Data { get; set; }
+    }
+
+
+}
